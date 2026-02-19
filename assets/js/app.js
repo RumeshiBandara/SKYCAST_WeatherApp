25f29fa617670e3116f73e169423f389c80790d4
Rumeshi Bandara <rumeshicharuni@gmail.com>
2026-02-19 22:42:04 +0530

    create async getWeatherData function with default city and loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
 const searchInput = document.getElementById('txtsearch');
 const loader = document.getElementById('loader');
 const weatherContent = document.getElementById('weather-content');
+
+ async function getWeatherData() {
+     const city = searchInput.value || "Colombo";
+     showLoader(true);
+
+
+ }
